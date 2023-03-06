1f6bc729ff60769af94e041d45800ee032d17c1a
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-03-06 18:06:39 +0100

    solution for task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * check_cycle - check if a linked list has cycles
+ *
+ * @list: head of linked list
+ *
+ * Return: 0 if there are no cycles; otherwise 1
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *temp;
+	listint_t *iter;
+
+	temp = list;
+	iter = list;
+
+	while (iter->next != NULL)
+	{
+		iter = iter->next;
+
+		if (iter == temp)
+			return (1);
+	}
+
+	return (0);
+}
