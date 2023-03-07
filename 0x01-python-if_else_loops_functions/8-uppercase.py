73a46f5d55bcbced3b64427bf361cc9862ac9cd0
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-03-07 21:36:51 +0100

    correction to task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,21 +5,23 @@ def uppercase(str):
     count = 0
     length = len(str)
 
-    if str == "":
-        print("")
+    if length == 0:
+        str = "EMPTY!"
 
     for i in str:
         ascii_val = ord(i)
 
         if ascii_val >= 97 and ascii_val <= 122:
             i = chr(ascii_val - 32)
-        if length == 0:
-            i = " "
 
-        if count < (length - 1):
+        if count < (length - 1) and str != "EMPTY!":
             print("{}".format(i), end="")
         else:
+            if str == "EMPTY!":
+                i = ""
+
             print("{}".format(i))
+            return 0
 
         count += 1
 
