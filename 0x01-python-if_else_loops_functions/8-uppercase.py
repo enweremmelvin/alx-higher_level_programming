f41b434b5b514e6b8daa61f49420dfbb38d4692f
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-03-07 17:27:24 +0100

    solution to task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+
+    count = 0
+    length = len(str)
+
+    for i in str:
+        ascii_val = ord(i)
+
+        if ascii_val >= 97 and ascii_val <= 122:
+            i = chr(ascii_val - 32)
+
+        if count < (length - 1):
+            print("{}".format(i), end="")
+        else:
+            print("{}".format(i))
+
+        count += 1
