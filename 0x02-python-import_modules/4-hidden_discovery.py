1edc3a637fda55470c6f0f3fc7c66374474d29c5
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-03-09 15:39:30 +0100

    solution to task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-
 import hidden_4
 
 names = dir(hidden_4)
 
-for i in names:
-    if i[0] != "_":
-        print(i)
+if __name__ == "__main__":
+    for i in names:
+        if i[0] != "_":
+            print(i)
