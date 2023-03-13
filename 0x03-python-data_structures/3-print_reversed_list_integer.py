02c905e282a955ffbeab31e264302d7251dddcfb
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-03-13 11:20:06 +0100

    correction for task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,10 @@
 
 def print_reversed_list_integer(my_list=[]):
 
+    if len(my_list) == 0:
+        print()
+        return
+
     list_len = (len(my_list) + 1) * -1
 
     for i in range(-1, list_len, -1):
